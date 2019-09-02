062f93b6b66b9311313b49ca26275376e9cce9bf
Hofer Benjamin <benjamin.hofer@ggs.ch>
2019-09-02 17:36:15 +0200

    Bugfixes

---
+++ MatrixClass/GenAlg.py
@@ -0,0 +1,39 @@
+from NeuralNetwork import NeuralNetwork
+from typing import List
+import random
+import copy
+
+class GenAlgo:
+
+    def __init__(self, amountOfNetworks, amountOfLayers, layerVec : List[int]):
+        self.Neurals = [NeuralNetwork(amountOfLayers, layerVec) for _ in range(amountOfNetworks)]
+        self.output = []
+    
+    def feedFoward(self, input : List[float]):
+        output = []
+        for net in self.Neurals:
+            output.append(net.feedForward(input))
+        return output
+
+    def nextGen(self, fit : List[float]):
+        sumOfVec = sum(fit)
+
+        ind = []
+        while len(ind) < len(self.Neurals)/2:
+            randchoice = random.randint(0, len(fit)-1)
+            if not randchoice in ind and random.uniform(0, 1) < fit[randchoice]:
+                ind.append(randchoice)
+
+        ind2 = []
+        for  i in range(len(self.Neurals)):
+            if not i in ind:
+                ind2.append(i)
+
+        for i in range(len(ind2)):
+           self.Neurals.pop(ind2[-i - 1])
+
+        tempVec = []
+        for net in self.Neurals:
+            tempVec.append(copy.deepcopy(net))
+            tempVec[-1].adjust()
+        self.Neurals += tempVec
--- MatrixClass/main.py
+++ MatrixClass/main.py
@@ -1,41 +1,31 @@
 from MatrixClass import Matrix
 from NeuralNetwork import NeuralNetwork
 from operator import itemgetter
+from GenAlg import GenAlgo
 import copy
 import statistics
 
-def getMean(numbers):
-    sum = 0
-    for el in numbers:
-        sum += el
-    sum /= len(numbers)
-    return sum
 
-networks = [NeuralNetwork(3, [2, 3, 1]) for _ in range(10)]
+
+networks = GenAlgo(10, 3, [2, 3, 1])
 
 while(True):
-    output = []
-    for network in networks:
-        output.append(network.feedForward([0, 1]))
+
+    output = networks.feedFoward([0, 1])
 
     fit = [max(min(1-abs(1-x[0]), 1), 0) for x in output]
     print(statistics.mean(fit))
 
     maxi = max(fit)
+    fit2 = copy.deepcopy(fit)
     ind = []
     for i in range(5):
         mini =  min(fit)
         ind.append(fit.index(mini))
         fit[ind[-1]] = maxi
         
-    ind.sort()
-    for i in range(5):
-        del networks[ind[-i-1]]
-
-    for i in range(5):
-        networks.append(copy.deepcopy(networks[i]))
-        networks[-1].adjust()
+    networks.nextGen(fit2)
 
     choice = input("Press a button")
     if choice == 'o':
-        print(output)
+       print(output)
