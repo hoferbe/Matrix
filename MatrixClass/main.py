e0d99d473f3dc019a10890e293c0af6ce2c34d90
hoferbe <hoferbe@NHOFERBE>
2019-09-05 10:40:01 +0200

    GenAlg verbesset und

--- MatrixClass/main.py
+++ MatrixClass/main.py
@@ -7,24 +7,16 @@ import statistics
 
 
 
-networks = GenAlgo(10, 3, [2, 3, 1])
+networks = GenAlgo(100, 3, [2, 3, 1])
 
 while(True):
 
     output = networks.feedFoward([0, 1])
 
-    fit = [max(min(1-abs(1-x[0]), 1), 0) for x in output]
+    fit = [max(min(x[0], 1), 0) for x in output]
     print(statistics.mean(fit))
-
-    maxi = max(fit)
-    fit2 = copy.deepcopy(fit)
-    ind = []
-    for i in range(5):
-        mini =  min(fit)
-        ind.append(fit.index(mini))
-        fit[ind[-1]] = maxi
         
-    networks.nextGen(fit2)
+    networks.nextGen(fit)
 
     choice = input("Press a button")
     if choice == 'o':
