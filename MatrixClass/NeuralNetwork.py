e747a870c188203b10701a7f25016b0ec00bab9d
Hofer Benjamin <benjamin.hofer@ggs.ch>
2019-09-13 14:33:47 +0200

    Dafuq?

--- MatrixClass/NeuralNetwork.py
+++ MatrixClass/NeuralNetwork.py
@@ -8,6 +8,8 @@ import random
 class NeuralNetwork:
     def __init__(self, layers : int, amountPerLayer : List[int]):
         self.network = []
+        self.adjustvalue = 0.1
+        self.counter = 0
         for i in range(layers-1):
             self.network.append(Matrix(amountPerLayer[i], amountPerLayer[i+1], []))
 
@@ -17,8 +19,12 @@ class NeuralNetwork:
         return input
 
     def adjust(self):
+        self.counter += 1
         for mat in self.network:
             adjustList = []
             for i in range(mat.width * mat.height):
-                adjustList.append(random.uniform(-0.1, 0.1))
+                adjustList.append(random.uniform(-self.adjustvalue, self.adjustvalue))
             mat.adjust(adjustList)
+        if self.counter > 10:
+            self.adjustvalue *= 0.9
+            self.counter = 0
