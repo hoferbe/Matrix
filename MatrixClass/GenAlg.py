3e246b41342c2530d5b9b2fc0b3d2d89243afdcf
Hofer Benjamin <benjamin.hofer@ggs.ch>
2019-09-13 14:38:33 +0200

    Merge

--- MatrixClass/GenAlg.py
+++ MatrixClass/GenAlg.py
@@ -16,14 +16,8 @@ class GenAlgo:
         return output
 
     def nextGen(self, fit : List[float]):
-        sumOfVec = 0
-        for el in fit:
-            sumOfVec += el
+        sumOfVec = sum(fit)
 
-        saveFit = copy.deepcopy(fit)
-        #for ind in range(len(fit)-1):
-            #fit[ind] /= sumOfVec
-        
         ind = []
         ind.sort()
         while len(ind) < len(self.Neurals)/2:
@@ -35,21 +29,6 @@ class GenAlgo:
         for  i in range(len(self.Neurals)):
             if not i in ind:
                 ind2.append(i)
-        ind2.sort()
-        sumSurvive = 0
-        sumDead = 0
-
-        for index in range(len(saveFit)):
-            if index in ind:
-                sumSurvive += saveFit[index]
-            elif index in ind2:
-                sumDead += saveFit[index]
-            else: print("WTF?")
-
-        print("Max fitness: " + str(max(saveFit)))
-        print("Min fitness: " + str(min(saveFit)))
-        print("Average survived fitness: " + str(sumSurvive/len(ind)) + " with a langth of " + str(len(ind)))
-        print("Average dead fitness: " + str(sumDead/len(ind2)) + " with a langth of " + str(len(ind2)))
 
         for i in range(len(ind2)):
            self.Neurals.pop(ind2[-i - 1])
